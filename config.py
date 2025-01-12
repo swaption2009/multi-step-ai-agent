06a07385b4dcfec5e0e4363776fc145576f942eb
Ronny Hoesada <swaption2009@gmail.com>
2025-01-12 15:02:45 -0800

    add credentials

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import os
+
 LLM_MODEL = "gemini-2.0-flash-exp"
-PROJECT_NUMBER=XXX
-SEARCH_ENGINE_ID="XXX"
-PROJECT_ID = "XXX"  # @param {type:"string"}
-BIGQUERY_DATASET_ID = "XXX"
-FINNHUB_API_KEY = "XXX"
+PROJECT_NUMBER = os.getenv("GCP_PROJECT_NUMBER")
+SEARCH_ENGINE_ID= os.getenv("GCP_SEARCH_ENGINE_ID")
+PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+BIGQUERY_DATASET_ID = os.getenv("GCP_BIGQUERY_DATASET_ID")
+FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY") 
+
 # ... other configuration values
 
 
